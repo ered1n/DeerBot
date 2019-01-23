ef1963f084640be15e82b33f2d50aa11b6bc6349
Angelo <ACabrasNL@gmail.com>
2019-01-24 00:00:42 +0100

    Added main bot file, updated .gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Bot online');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase()
+
+	if (command === 'ping') {
+        message.channel.send('Pong.');
+    }
+});
+
+client.login(token);
