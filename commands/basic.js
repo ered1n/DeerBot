ca1b2bfe2252930f8201a76192e3de5d489dcee1
Angelo <ACabrasNL@gmail.com>
2019-01-24 01:21:48 +0100

    Added a ping command and emoji replies

---
+++ commands/basic.js
@@ -0,0 +1,15 @@
+/*
+	Basic commands
+*/
+
+module.exports = {
+
+	//  Sends delay between bot and servers in ms
+	name: 'ping',
+	description: 'Check the bot\'s ping',
+	execute(client, message, args) {
+		message.channel.send('Pong! :ping_pong:```' + client.ping + 'ms```');
+	},
+	
+
+};
--- index.js
+++ index.js
@@ -1,19 +1,82 @@
+/*
+    DeerBot is a Discord bot made by Eredin#2903 & Angelo#1337
+
+                     /|       |\
+                  `__\\       //__'
+                     ||      ||
+                   \__`\     |'__/
+                     `_\\   //_'
+                     _.,:---;,._
+                     \_:     :_/
+                       |@. .@|
+                       |     |
+                       ,\.-./ \
+                       ;;`-'   `---__________-----.-.
+                       ;;;                         \_\
+                       ';;;                         |
+                        ;    |                      ;
+                         \   \     \        |      /
+                          \_, \    /        \     |\
+                            |';|  |,,,,,,,,/ \    \ \_
+                            |  |  |           \   /   |
+                            \  \  |           |  / \  |
+                             | || |           | |   | |
+                             | || |           | |   | |
+                             | || |           | |   | |
+                             |_||_|           |_|   |_|
+                            /_//_/           /_/   /_/
+
+    If you are reading this, please stop yoinking our code.
+*/
+
+//  Imports
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const {prefix, token} = require('./config.json');
 
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+//  Load command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+//  Runs on bot startup
 client.once('ready', () => {
-	console.log('Bot online');
+    console.log('Bot online');
+    client.user.setActivity('deer', {type: 'WATCHING'});
 });
 
+//  Listen to messages sent from client
 client.on('message', message => {
+
+    //  Reacts with emoji if message contains a certain word
+    if (message.content.toLowerCase().includes("deer")) { message.react('🦌'); }  
+    if (message.content.toLowerCase().includes("cat")) { message.react('🐱'); }
+    if (message.content.toLowerCase().includes("dog")) { message.react('🐶'); }
+    if (message.content.toLowerCase().includes("snek")) { message.react('🐍'); }
+    if (message.content.toLowerCase().includes("duck")) { message.react('🦆'); }
+    if (message.content.toLowerCase().includes("bear")) { message.react('🐻'); }
+    //if (message.content.toLowerCase().includes("nigger")) { message.react('HYPERBRUH'); }
+    //if (message.content.toLowerCase().includes("gnome")) { message.react('GNOMED'); }
+
+    //  Return if message doesn't start with prefix or author is bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
+
+    //  Get command and arguments
     const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase()
 
-	if (command === 'ping') {
-        message.channel.send('Pong.');
+    //  Execute command
+	try {
+        client.commands.get(command).execute(client, message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
     }
 });
 
